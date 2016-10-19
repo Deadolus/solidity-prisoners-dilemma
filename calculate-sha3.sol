c0055495e7ad27879b5d96acb0f2eebe4f5dfcd8
Simon Egli <simon.egli@bbv.ch>
2016-10-19 14:31:35 +0200

    better calculate-sha3 contract
    Also added some more information to it
    
    Signed-off-by: Simon Egli <simon.egli@bbv.ch>

--- calculate-sha3.sol
+++ calculate-sha3.sol
@@ -1,24 +1,26 @@
+pragma solidity ^0.4.0;
+
 ///Hashing contract to calculate choice hash. 
 ///Input this at e.g. https://ethereum.github.io/browser-solidity/
-contract HashTest {
-    enum Choice { Betray, Silence }
-    function testSha3() returns (bytes32) {
-
-        Choice choice = Choice.Betray;
-        uint32 random = 2;
-
-        return sha3(choice, random); 
+contract GenerateCoiceHash {
+    enum Choice { None, Betray, Silence }
+    function generateHash(Choice _choice, bytes32 _random) returns (bytes32) {
+        if(_choice>Choice.Silence)
+        throw;
+        return sha3(_choice, _random); 
     }
 }
 
-//betray, 2: 0xb4c9eaf404872994677d9def95dee3fe36bfbcd9be2312670ef7be131a502f32
-//silence, 2: 0x8fa5e686e43185c105c30bbebcd43116da0ad7d255786c11dd777fd944dfaa8b
+//betray(1), 2: 0x8fa5e686e43185c105c30bbebcd43116da0ad7d255786c11dd777fd944dfaa8b
+// is equal to 64973858151419717045263467517550949389636568819796684286811224219562127895179
+//silence(2), 2: 0xc6ef2da351a8bfdbb5444c07de5fd9277923b7cfd84716394a6c231593268c04
+// is equal to 89980535449625406194470317847071652620265794397825794665227851275585871842308
 
-//betray, 0: 0xc41589e7559804ea4a2080dad19d876a024ccb05117835447d72ce08c1d020ec
-//silene, 0:0x0cb66f6e05387bcfc613a09c9a3fadb03218d2222342a3037275b1a196a6a520
+//betray(1), 0: 0x0d678e31a4b2825b806fe160675cd01dab159802c7f94397ce45ed91b5f3aac6
+//silene(2), 0: 0x5da513e113e3f2fd0c7f9fdb338fc156917b82fe159806cc152be5bba89d8e7b
 
-//Silence: 0x0000000000000000000000000000000000000000000000000000000000000001
-//Betray: 0x0000000000000000000000000000000000000000000000000000000000000000
+//Silence: 0x0000000000000000000000000000000000000000000000000000000000000002
+//Betray: 0x0000000000000000000000000000000000000000000000000000000000000001
 
 /*
 contract HashTestWithString {
